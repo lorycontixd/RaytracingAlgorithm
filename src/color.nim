4a679a16cdf1acd3096dde0b6aaf8ca1bd6a61e6
loryconti <loryconti1@gmail.com>
2022-03-10 10:29:52 +0100

    Bug Fix + Test correction
    Bug Fix: hdr.set_pixel needs a var HdrImage for the pixels to be mutable
    Test Correction:
    - Added more tests for colors
    - Added more tests for HdrImage.
    - Removed unnecessary testing nimble files

--- src/color.nim
+++ src/color.nim
@@ -11,18 +11,18 @@ proc newColor*(r,g,b: float32): Color=
 proc newColor*(color: Color): Color=
     result = color
 
-proc `+`*(c1,c2: Color): Color=
+proc `+`*(c1,c2: Color): Color {.inline.}=
     return Color(r: c1.r+c2.r, g: c1.g+c2.g, b: c1.b+c2.b)
 
-proc `*`*(c1,c2: Color): Color=
+proc `*`*(c1,c2: Color): Color {.inline.}=
     return Color(r: c1.r*c2.r, g: c1.g*c2.g, b: c1.b*c2.b)
 
-proc `*`*(c1: Color, a:float): Color=
+proc `*`*(c1: Color, a:float): Color {.inline.}=
     return Color(r: c1.r*a, g: c1.g*a, b: c1.b*a)
 
-proc IsEqual(x,y: float32, epsilon:float32=1e-5): bool =
+proc IsEqual(x,y: float32, epsilon:float32=1e-5): bool {.inline.}=
     return abs(x - y) < epsilon
 
-proc `==`*(c1,c2: Color): bool =
+proc `==`*(c1,c2: Color): bool {.inline.}=
     return IsEqual(c1.r, c2.r) and IsEqual(c1.g, c2.g) and IsEqual(c1.b, c2.b)
 
--- tests/test_color.nim
+++ tests/test_color.nim
@@ -1,11 +1,12 @@
+import unittest
 import "../src/color.nim"
 
 let c1=Color(r:1,g:2,b:3)
 let c2=Color(r:4,g:5,b:6)
-echo c1+c2
-echo c1*2
-
 
 #funzioni supporto ai test
 assert c1+c2 == newColor(5,7,9)
 assert c1*2 == newColor(2,4,6)
+assert c1 == newColor(1,2,3)
+assert c2 == newColor(4,5,6+1e-6)
+
