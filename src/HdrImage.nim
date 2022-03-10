d41a89a0c235418b5499b08ef5bd54d724bec8e8
loryconti <loryconti1@gmail.com>
2022-03-10 10:29:52 +0100

    Bug Fix + Test correction
    Bug Fix: hdr.set_pixel needs a var HdrImage for the pixels to be mutable
    Test Correction:
    - Added more tests for colors
    - Added more tests for HdrImage.
    - Removed unnecessary testing nimble files

--- src/HdrImage.nim
+++ src/HdrImage.nim
@@ -8,22 +8,27 @@ type
 
 proc newHdrImage*(width, height: int): HdrImage=
     result = HdrImage(width: width, height: height, pixels: newSeq[Color](width*height))
-    for i in 0..width*height:
-        result.pixels[i]= newColor(0,0,0)
+    for i in 0..width*height-1:
+        result.pixels[i] = newColor(0,0,0)
 
-proc valid_coordinates(z: HdrImage, x,y:int): bool=
-    return((x>=0) and (x<z.width) and (y>=0) and (y<z.height))
+proc newHdrImage*(other:HdrImage): HdrImage {.inline.}=
+    result = HdrImage(width:other.width, height:other.height, pixels:other.pixels)
 
-proc pixel_offset*(z: HdrImage, x,y:int): int {.inline.}=
-    result = y * z.width + x
 
-proc get_pixels(z: HdrImage, x,y:int): Color=
-    assert z.valid_coordinates(x,y)
-    return z.pixels[z.pixel_offset(x,y)]
+proc valid_coordinates*(self: HdrImage, x,y:int): bool=
+    result = ((x>=0) and (x<self.width) and (y>=0) and (y<self.height))
 
-proc set_pixels(z: HdrImage, x,y:int, new_color: var Color) = 
-    assert z.valid_coordinates(x,y)
-    z.pixels[z.pixel_offset(x,y)] = newColor(new_color)
+proc pixel_offset*(self: HdrImage, x,y:int): int {.inline.}=
+    result = y * self.width + x
+
+proc get_pixel*(self: HdrImage, x,y:int): Color=
+    assert self.valid_coordinates(x,y)
+    result = self.pixels[self.pixel_offset(x,y)]
+
+proc set_pixel*(self: var HdrImage, x,y:int, new_color: Color)= 
+    assert self.valid_coordinates(x,y)
+    let offset = self.pixel_offset(x,y)
+    self.pixels[offset] = new_color
 
 
         
