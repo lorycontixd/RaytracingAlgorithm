4e5f51481f4c069d3c7ce893ad4c2f1fb011ef25
gigabit-studios <loryconti1@gmail.com>
2022-03-18 23:56:14 +0100

    HdrImage + Color + Exception update

--- src/utils.nim
+++ src/utils.nim
@@ -1,13 +1,11 @@
-import std/[strutils, strformat]
+import std/[strutils, strformat, macros]
 from macros import newTree, nnkBracket, newLit
 from sequtils import mapIt
 
 
-proc hexDump*[T](v: T): string =
-  var s: seq[uint8] = @[]
-  s.setLen(v.sizeof)
-  copymem(addr(s[0]), v.unsafeAddr, v.sizeof)
-  result = ""
+proc toString*(bytes: openarray[byte]): string =
+  result = newString(bytes.len)
+  copyMem(result[0].addr, bytes[0].unsafeAddr, bytes.len)
 
 proc byteArrayToHex32*(a: array[4,byte]): string {.inline.}=
     var b: array[4, string]
@@ -16,12 +14,19 @@ proc byteArrayToHex32*(a: array[4,byte]): string {.inline.}=
     result = fmt"{b[0]}{b[1]} {b[2]}{b[3]}"
 
 proc seqToArray32*(s: seq[byte]): array[4, byte] {.inline.} =
-  assert len(s)==4
-  var x: array[4, byte]
-  for i in 0..len(s)-1:
-    x[i] = s[i]
-  return x
+    assert len(s)==4
+    var x: array[4, byte]
+    for i in 0..len(s)-1:
+        x[i] = s[i]
+    return x
 
-proc concatArrays[I1, I2: static[int]; T](a: array[I1, T], b: array[I2, T]): array[I1 + I2, T] =
-  result[0..a.high] = a
-  result[a.len..result.high] = b
+proc size*[T](x: T): int =
+    for _ in x:
+        inc result
+
+proc charSeqToByte*(s: seq[char]): seq[byte] {.inline.}= 
+    echo size(s)
+    result = newSeq[byte](size(s))
+    for i in 0..size(s)-1:
+        result[i] = cast[byte](s[i])
+
--- tests/test_hdrimage.nim
+++ tests/test_hdrimage.nim
@@ -1,22 +1,10 @@
 import unittest
-import streams
+import std/streams
 import "../src/hdrimage.nim"
 import "../src/color.nim"
-type Endianness = hdrimage.Endianness
 
 var hdr = newHdrImage(1000,100)
 assert hdr.valid_coordinates(1,1)
 #assert hdr.valid_coordinates(-1,1) # correctly failed
 #assert hdr.valid_coordinates(21,1) # correctly failed
 #assert hdr.valid_coordinates(1,11) # correctly failed
-
-
-var hdr1 = newHdrImage(120,120,Endianness.littleEndian)
-#assert endianTable[hdr1.endianness] == "<f"
-assert hdr1.endianness == Endianness.littleEndian
-
-var readstrm : FileStream = newFileStream("images/memorial.pfm", fmRead)
-var writestrm : FileStream = newFileStream("images/res1.pfm", fmWrite)
-
-var hdr2 = hdr.read_pfm(readstrm)
-hdr.write_black(writestrm)
