572d65ffbc29ced3c5f5ef8ecdc52900ebc18540
gigabit-studios <loryconti1@gmail.com>
2022-03-30 15:58:37 +0200

    Added constructors + some operators on geometry

--- src/RaytracingAlgorithm/geometry.nim
+++ src/RaytracingAlgorithm/geometry.nim
@@ -1,4 +1,5 @@
 import neo
+import std/[math, macros, typetraits]
 
 type
     Vector* = object
@@ -13,6 +14,26 @@ type
     Transformation* = object
         m*, inverse*: Matrix[float32]
 
+## --- Constructors
+
+template define_constructors*(fname: untyped, type1: typedesc) =
+    proc fname*(): type1=
+        ## Empty constructor for Vector, point & Normal
+        result = type1(x:0, y:0, z:0)
+
+    proc fname*(x,y,z: float32): type1 =
+        ## (x,y,z) constructor for Vector, Point & Normal
+        result = type1(x:x, y:y, z:z)
+
+    proc fname*(other: type1): type1 =
+        ## Copy constructor for Vector, Point & Normal
+        result = type1(x:other.x, y:other.y, z: other.z)
+
+define_constructors(`newVector`, Vector)
+define_constructors(`newPoint`, Point)
+define_constructors(`newNormal`, Normal)
+
+## --- Sum & Subtraction
 
 template define_operations(fname: untyped, type1: typedesc, type2: typedesc, rettype: typedesc) =
     proc fname*(a: type1, b: type2): rettype =
@@ -29,6 +50,27 @@ define_operations(`-`, Point, Vector, Point)
 define_operations(`+`, Normal, Normal, Normal)
 define_operations(`-`, Normal, Normal, Normal)
 
+## --- Products
+
 template define_product(type1: typedesc, type2: typedesc, rettype: typedesc) =
     proc `*`*(a: type1, b: type2): float32 =
-        result = a.x*
+        result = a.x * b.x + a.y * b.y + a.z * b.z
+    
+    proc `@`*(a: type1, b: type2): rettype =
+        result.x = a.y * b.z - a.z * b.y
+        result.y = a.z * b.x - a.x * b.z
+        result.z = a.x * b.y - a.y * b.x
+
+
+define_product(Vector, Vector, Vector)
+define_product(Point, Vector, Vector)
+
+## --- Norm
+
+template define_norm(type1: typedesc)=
+    proc norm*(a: type1): float32=
+        result = sqrt(pow(a.x,2) + pow(a.y,2) + pow(a.z,2))
+    
+define_norm(Vector)
+define_norm(Point)
+
--- tests/test_geometry.nim
+++ tests/test_geometry.nim
@@ -1,6 +1,5 @@
 import "../src/RaytracingAlgorithm/geometry.nim"
 
-var a= Vector(x: 3.0, y: 2.0, z: 1.0)
-var b= Vector(x: -3.0, y: 5.0, z: -7.0)
-var c= a+b
-assert c == Vector(x: 0.0, y: 7.0, z: -6.0)
+var a = Vector(x: 3.0, y: 2.0, z: 1.0)
+var b = Vector(x: -3.0, y: 5.0, z: -7.0)
+assert a+b == Vector(x: 0.0, y: 7.0, z: -6.0)
