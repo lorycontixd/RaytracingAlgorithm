5f69a5b76cd8c41e31f58b74683b884bd9e08828
DianaBarindelli <diana.barindelli@gmail.com>
2022-03-30 12:51:23 +0200

    adding functionalities to geometry

--- src/RaytracingAlgorithm/geometry.nim
+++ src/RaytracingAlgorithm/geometry.nim
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import neo
 
 type
@@ -13,3 +12,23 @@ type
 
     Transformation* = object
         m*, inverse*: Matrix[float32]
+
+
+template define_operations(fname: untyped, type1: typedesc, type2: typedesc, rettype: typedesc) =
+    proc fname*(a: type1, b: type2): rettype =
+        result.x = fname(a.x, b.x)
+        result.y = fname(a.y, b.y)
+        result.z = fname(a.z, b.z)
+
+define_operations(`+`, Vector, Vector, Vector)
+define_operations(`-`, Vector, Vector, Vector)
+define_operations(`+`, Vector, Point, Point)
+define_operations(`-`, Vector, Point, Point)
+define_operations(`+`, Point, Vector, Point)
+define_operations(`-`, Point, Vector, Point)
+define_operations(`+`, Normal, Normal, Normal)
+define_operations(`-`, Normal, Normal, Normal)
+
+template define_product(type1: typedesc, type2: typedesc, rettype: typedesc) =
+    proc `*`*(a: type1, b: type2): float32 =
+        result = a.x*
