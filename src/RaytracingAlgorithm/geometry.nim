7ff1dd1a6f7b82fd1dbdfbc9e4e5cff7f2661a2b
Lorenzo <loryconti1@gmail.com>
2022-03-30 18:04:21 +0200

    Defined constructors + operators

--- src/RaytracingAlgorithm/geometry.nim
+++ src/RaytracingAlgorithm/geometry.nim
@@ -16,22 +16,36 @@ type
 
 ## --- Constructors
 
-template define_constructors*(fname: untyped, type1: typedesc) =
-    proc fname*(): type1=
-        ## Empty constructor for Vector, point & Normal
-        result = type1(x:0, y:0, z:0)
+macro define_empty_constructors(type1: typedesc): typed =
+    let source = fmt"""
+proc new{$type1}(): {$type1} =
+    result = {$type1}(x: 0.0, y: 0.0, z: 0.0)
+"""
+    result = parseStmt(source)
 
-    proc fname*(x,y,z: float32): type1 =
-        ## (x,y,z) constructor for Vector, Point & Normal
-        result = type1(x:x, y:y, z:z)
+macro define_constructors(type1: typedesc): typed =
+    let source = fmt"""
+proc new{$type1}(x,y,z: float32): {$type1} =
+    result = {$type1}(x: x, y: y, z: z)
+"""
+    result = parseStmt(source)
 
-    proc fname*(other: type1): type1 =
-        ## Copy constructor for Vector, Point & Normal
-        result = type1(x:other.x, y:other.y, z: other.z)
+macro define_copy_constructors(type1: typedesc): typed =
+    let source = fmt"""
+proc new{$type1}(other: {$type1}): {$type1} =
+    result = {$type1}(x: other.x, y: other.y, z: other.z)
+"""
+    result = parseStmt(source)
 
-define_constructors(`newVector`, Vector)
-define_constructors(`newPoint`, Point)
-define_constructors(`newNormal`, Normal)
+define_empty_constructors(Point)
+define_empty_constructors(Vector)
+define_empty_constructors(Normal)
+define_constructors(Point)
+define_constructors(Vector)
+define_constructors(Normal)
+define_copy_constructors(Point)
+define_copy_constructors(Vector)
+define_copy_constructors(Normal)
 
 ## --- Sum & Subtraction
 
@@ -55,15 +69,8 @@ define_operations(`-`, Normal, Normal, Normal)
 template define_product(type1: typedesc, type2: typedesc, rettype: typedesc) =
     proc `*`*(a: type1, b: type2): float32 =
         result = a.x * b.x + a.y * b.y + a.z * b.z
-    
-    proc `@`*(a: type1, b: type2): rettype =
-        result.x = a.y * b.z - a.z * b.y
-        result.y = a.z * b.x - a.x * b.z
-        result.z = a.x * b.y - a.y * b.x
 
 
-define_product(Vector, Vector, Vector)
-define_product(Point, Vector, Vector)
 
 ## --- Norm
 
