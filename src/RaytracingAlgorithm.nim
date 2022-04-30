9ea0bbee49cf34d8f68a422236cb51669072490c
gigabit-studios <loryconti1@gmail.com>
2022-04-30 22:28:11 +0200

    main update

--- src/RaytracingAlgorithm.nim
+++ src/RaytracingAlgorithm.nim
@@ -13,7 +13,22 @@ when isMainModule:
         vertex_spheres: seq[Sphere] = newSeq[Sphere](8)
         #imagetracer: ImageTracer = newImageTracer()
 
-let args = (
-    name: newStringArg(@["<command>"], help="Command to execute")
+
+let cube = (
+    width: newIntArg(@["--width"], defaultVal=800, help="Width of the image to render"),
+    height: newIntArg(@["--height"], defaultVal=600, help="Height of the image to render"),
+    pfm_output: newBoolArg(@["--pfm_output"], defaultVal=false, help="Produce a ")
 
 )
+
+let demo = (
+    cube: newCommandArg(@["cube"], create, help="Create a new ship"),
+)
+
+let spec = (
+      ship: newCommandArg(@["demo"], demo, help="Ship commands"),
+      mine: newCommandArg(@["render"], mine, help="Mine commands"),
+      help: newHelpArg()
+)
+
+
