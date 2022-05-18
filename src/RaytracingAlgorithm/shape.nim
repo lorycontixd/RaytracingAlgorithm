d6f0c0d016033313c41839a0d76e3b35ffd73a63
gigabit-studios <loryconti1@gmail.com>
2022-05-18 09:54:05 +0200

    Cylinder start

--- src/RaytracingAlgorithm/shape.nim
+++ src/RaytracingAlgorithm/shape.nim
@@ -42,6 +42,10 @@ proc newPlane*(id: string = "PLANE_0", transform: Transformation = newTransforma
         raise ValueError.newException("Plane id must contain PLANE keyword.")
     result = Plane(id: id, transform: transform, material: material, origin: ExtractTranslation(transform.m).convert(Point))
 
+proc newCylinder*(id: string = "CYLINDER_0", transform: Transformation, material: Material = newMaterial()): Cylinder=
+    if not id.contains("CYLINDER"):
+        raise ValueError.newException("Cylinder id must contain CYLINDER keyword.")
+    result = Cylinder(id: id, transform: transform, material: material, aabb: newAABB(newPoint(), newPoint()))
 # -------------------------------------- Private methods ------------------------------------
 proc sphereNormal(p: Point, dir: Vector3): Normal= 
     var n: Normal = newNormal(p.x, p.y, p.z)
@@ -51,6 +55,15 @@ proc sphereNormal(p: Point, dir: Vector3): Normal=
         return n.neg()
 
 proc sphereWorldToLocal(p: Point): Vector2=
+    let
+        u = arctan2(p.y, p.z) / (2.0 * PI)   #p.x not p.z ??
+        v = arccos(p.z) / PI ## divided by radius ??
+    if u >= 0.0:
+        result = newVector2(u,v)
+    else:
+        result = newVector2(u+1.0, v)
+
+proc cylinderWorldToLocal(p: Point): Vector2 =
     let
         u = arctan2(p.y, p.z) / (2.0 * PI)   #p.x not p.z ??
         v = arccos(p.z) / PI ## divided by radius ??
@@ -81,7 +94,7 @@ method rayIntersect*(s: Sphere, r: Ray, debug: bool = false): Option[RayHit] {.g
     var
         a = inversed_ray.dir.squareNorm()
         b = 2.0 * origin_vec.Dot(inversed_ray.dir) 
-        c = origin_vec.squareNorm() - 1
+        c = origin_vec.squareNorm() - s.radius * s.radius
     
         delta = b * b - 4.0 * a * c
 
@@ -138,5 +151,43 @@ method rayIntersect*(self: Plane, ray: Ray, debug: bool = false): Option[RayHit]
         self.material,
     ))
     
+method rayIntersect*(self: Cylinder, ray: Ray, debug: bool = false): Option[RayHit]=
+    var hit: RayHit = newRayHit()
+    var
+        firsthit_t: float32
+        inversed_ray: Ray = ray.Transform(self.transform.Inverse())
+        origin_vec: Vector3 = inversed_ray.origin.convert(Vector3)
 
-    
+    #if not s.aabb.RayIntersect(inversed_ray):
+    #    return none(RayHit)
+
+    var
+        a = pow(inversed_ray.dir.x,2.0) 
+        b = 2.0 * (inversed_ray.dir.x * origin_vec.x + inversed_ray.dir.y * origin_vec.y)
+        c = pow(origin_vec.x, 2.0) + pow(origin_vec.y, 2.0) - 1 # - radius ^ 2
+        delta = b * b - 4.0 * a * c
+
+    if delta <= 0.0:
+        return none(RayHit)
+
+    let
+        sqrt_delta = sqrt(delta)
+        tmin = (- b - sqrt_delta) / (2.0 * a)
+        tmax = (- b + sqrt_delta) / (2.0 * a)
+
+    if (tmin > inversed_ray.tmin and tmin < inversed_ray.tmax):
+        firsthit_t = tmin
+    elif (tmax > inversed_ray.tmin and tmax < inversed_ray.tmax):
+        firsthit_t = tmax
+    else:
+        return none(RayHit)
+
+    let hitpoint = inversed_ray[firsthit_t]
+    hit.world_point = self.transform * hitpoint
+    hit.t = firsthit_t
+    #hit.normal = self.transform * sphereNormal(hitpoint, inversed_ray.dir) 
+    hit.surface_point = cylinderWorldToLocal(hitpoint)
+    hit.ray = ray
+    hit.material = self.material
+    #hit.hitshape = s
+    result = some(hit)
