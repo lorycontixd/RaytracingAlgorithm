37352a4dc905250b00b281c90fe5f0000faab59b
DianaBarindelli <diana.barindelli@gmail.com>
2022-06-19 23:15:07 +0200

    .

--- src/RaytracingAlgorithm/renderer.nim
+++ src/RaytracingAlgorithm/renderer.nim
@@ -9,7 +9,6 @@ from utils import injectProcName
 from ray import Ray
 from exception import NotImplementedError, AbstractMethodError
 import std/[options, math, times, typetraits]
-import weave
 
 type
     Renderer* = ref object of RootObj # abstract class to solve rendering equation
@@ -121,34 +120,38 @@ method Get*(renderer: FlatRenderer): (proc(r: Ray): Color) {.injectProcName.}=
         return ( brdfColor + emittedRadianceColor )
 
 
-method Get*(renderer: PathTracer): (proc(ray: Ray): Color) {.gcsafe, injectProcName.} = 
-    return proc(ray: Ray):Color=
+method Get*(renderer: PathTracer): (proc(ray: Ray): Color) {.gcsafe, injectProcName.} =
+    ## Returns the color of the renderer hit by ray
+    ## Parameters
+    ##      renderer (PathTracer)
+    ## Returns
+    ##      (Color)
+    return proc(ray: Ray): Color=
+        let start = now()
         if ray.depth > renderer.maxRayDepth:
             return Color.black()
-        
-        let hit_record = renderer.world.rayIntersect(ray)
-        if hit_record == none(RayHit):
+        let hitrecord = renderer.world.rayIntersect(ray)
+        if hitrecord == none(RayHit):
             return renderer.backgroundColor
-  
+        
+        let hit = hitrecord.get()
+        var hit_color: Color = hit.material.brdf.pigment.getColor(hit.GetSurfacePoint())
         let
-            hit = hitrecord.get()
-            hit_material = hit.material
-        var
-            hit_color: Color = hit.material.brdf.pigment.getColor(hit.GetSurfacePoint())
-            emitted_radiance: Color = hit.material.emitted_radiance.getColor(hit.GetSurfacePoint())
+            emitted_radiance = hit.material.emitted_radiance.getColor(hit.GetSurfacePoint())
             hit_color_lum = max(hit_color.r, max(hit_color.g, hit_color.b))
-
         # Russian roulette
         if ray.depth >= renderer.russianRouletteLimit:
             let q = max(0.05, 1 - hit_color_lum)
             if renderer.pcg.random_float() > q:
-                hit_color = hit_color * (1.0 / (1 - hit_color_lum))
+                #hit_color = hit_color * (1.0 / (1 - hit_color_lum))
+                hit_color = hit_color * (1.0 / (1 - q))
             else:
                 return emitted_radiance
-
+        # Monte Carlo integration
         var cum_radiance: Color = Color.black()
-        if hit_color_lum > 0.0:  # Only do costly recursions if it's worth it
-            for ray_index in 0..<renderer.numRays:
+        if hit_color_lum > 0.0:
+            #countup(0, renderer.numRays)
+            for ray_index in 0 || renderer.numRays:
                 let newRay = hit.material.brdf.ScatterRay(
                     renderer.pcg,
                     hit.ray.dir,
@@ -156,47 +159,12 @@ method Get*(renderer: PathTracer): (proc(ray: Ray): Color) {.gcsafe, injectProcN
                     hit.normal,
                     ray.depth + 1
                 )
-                # Recursive call
                 let newRadiance = renderer.Get()(newRay)
-                cum_radiance = cum_radiance + hit_color * new_radiance
-        return (emitted_radiance + cum_radiance * (1.0 / renderer.numRays.float32))
-
-
-#[
-method Get*(renderer: PathTracer): (proc(ray: Ray): Color) {.gcsafe, injectProcName.} = 
-    return proc(ray: Ray): Color=
-        # We need the buffer raw address
-        let buf = cast[ptr UncheckedArray[Vec]](C[0].addr)
-
-        init(Weave)
-
-        parallelFor y in 0 ..< h:               # Loop over image rows
-            captures: {buf, samples}
-            parallelFor x in 0 ..< w:             # Loop over columns
-                captures: {y, buf, samples}
-                var xi = [cushort 0, 0, cushort y*y*y]
-                let i = (h-y-1)*w+x
-                for sy in 0 ..< 2:                  # 2x2 subpixel rows
-                for sx in 0 ..< 2:                # 2x2 subpixel cols
-                    var r = vec()
-                    for s in 0 ..< samples:
-                    loadBalance(Weave)            # If we are tracing a complex ray, ensure we delegate loop iterations
-                    let
-                        r1 = 2*erand48(xi)
-                        dx = if r1<1: sqrt(r1)-1 else: 1-sqrt(2-r1)
-                        r2 = 2*erand48(xi)
-                        dy = if r2<1: sqrt(r2)-1 else: 1-sqrt(2-r2)
-                        d = cx*(((sx.float64 + 0.5 + dx)/2 + x.float64)/w - 0.5) +
-                            cy*(((sy.float64 + 0.5 + dy)/2 + y.float64)/h - 0.5) + cam.d
-                    let dnorm = d.norm() # Warning, the original code is deceptive since d is modified by d.norm()
-                    let ray = ray(cam.o + dnorm*140'f64, dnorm)
-                    let rad = radiance(ray, depth = 0, xi)
-                    r = r + rad * (1.0/samples.float64)
-                    buf[i] = buf[i] + vec(r.x.clamp(), r.y.clamp(), r.z.clamp()) * 0.25 # / num subpixels
-
-            exit(Weave) # This implicitly blocks until tasks are all done
-]#
-
+                cum_radiance = cum_radiance + hit_color * newRadiance
+                renderer.raysShot  = renderer.raysShot + 1
+        let endTime = now() - start
+        mainStats.AddCall(procName, endTime, 1)
+        return emitted_radiance + cum_radiance * (1.0 / float32(renderer.numRays))
 
 method Get*(self: PointlightRenderer): (proc(ray: Ray): Color) {.injectProcName.}=
     ## Returns the color of the renderer hit by ray
